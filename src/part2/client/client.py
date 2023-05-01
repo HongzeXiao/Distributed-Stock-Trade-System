f73556ff8a6a1988bb046208e3f49c3be1f850a8
HongzeXiao <124698379+HongzeXiao@users.noreply.github.com>
2023-05-01 17:19:44 -0400

    connect to aws

--- src/part2/client/client.py
+++ src/part2/client/client.py
@@ -1,15 +1,36 @@
 import requests, json, random, time, threading
 from ip import *
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 stockNames = ["GameStart", "FishCo", "BearCo", "MenhirCo", "Cici", "GameStart2", "FishCo2", "BearCo2", "MenhirCo2", "Cici2"]
 threshold = 0.5
-num_order = 0
-max_tran = 0
 
-lock = threading.Lock()
+class ClientState:
+    def __init__(self) -> None:
+        self.num_order = 0
+        self.max_tran = 0
+        self.lock = threading.Lock()
+
+    def increment_order(self):
+        with self.lock:
+            self.num_order += 1
+    def update_maxtran(self, max_tran):
+        with self.lock:
+            self.max_tran = max(max_tran, self.max_tran)
+    def get_order(self):
+        with self.lock:
+            return self.num_order
+
+    def get_tran(self):
+        with self.lock:
+            return self.max_tran
+    
+MyState = ClientState()
+
 # return transaction number on success 
 # -1 otherwise
 def order(session: requests.Session, stockName: str, max_vol: int)->int:
+    print("order start")
+
     frontend_url = f"{frontend_ip}/orders"
     type = "buy"
     quantity = random.randrange(0, min(max_vol - 1, 100))
@@ -22,13 +43,21 @@ def order(session: requests.Session, stockName: str, max_vol: int)->int:
         "type": type,
         "quantity": quantity
     }
-    reply = session.post(frontend_url, json=message).json()
+    try:
+        reply = session.post(frontend_url, json=message).json()
+    except Exception as e:
+        print(e)
+
     transaction_num = 0
     if ("error" in reply):
-        transaction_num = -1
+        transaction_num = 0
     else:
-        transaction_num = reply["data"]["transaction_number"]
-    return transaction_num
+        transaction_num = int(reply["data"]["transaction_number"])
+    print("order success")
+    global MyState
+    MyState.increment_order()
+    MyState.update_maxtran(transaction_num)
+    return int(transaction_num)
 
 # return remaining volume of the stockName
 # -1 on failure
@@ -40,38 +69,42 @@ def lookup(session: requests.Session, stockName: str)->int:
         vol = -1
     else:
         vol = reply["data"]["quantity"] 
-    return vol
+    return int(vol)
 
 def randomReq():
     stockName = random.choice(stockNames)
     session = requests.Session()
     vol = lookup(session, stockName)
-    global threshold
     if (vol > 10 and random.random() < threshold):
-        transaction = order(session, stockName, vol)
-        with lock:
-            global num_order, max_tran
-            max_tran = max(max_tran, transaction)
-            num_order += 1
+        _ = order(session, stockName, vol)
+
             
 def req(num_req):
     for i in range(num_req):
         randomReq()
         
 if __name__ == "__main__":
-    number_req = 200
+    number_req = 20
     start_time = time.time()
     max_worker = 5
     tasks = []
-    with ProcessPoolExecutor(max_worker) as executor:
+    with ThreadPoolExecutor(max_worker) as executor:
         for i in range(max_worker):
             tasks.append(executor.submit(req, number_req))
     
     for task in tasks:
-        task.result()
+        try:
+            task.result()
+        except Exception as e:
+            pass
         
     end_time = time.time()
+    # global max_tran, num_order
+    num_order = MyState.get_order()
+    max_tran = MyState.get_tran()
+    print(max_tran, num_order)
     total_req = number_req * max_worker + num_order
+
     print(f"Threshold: {threshold} | Number of Worker {max_worker}")
-    print(f"Send {total_req} requests ({number_req * max_worker} lookup and {num_order} orders) in {end_time - start_time} sec")
+    print(f"Send {total_req} requests ({number_req * max_worker} lookup; {num_order} orders; {max_tran} transactions) in {end_time - start_time} sec")
     print(f"Throughput: {total_req / (end_time - start_time)} req/sec | Latency: {(end_time - start_time) / total_req }")
--- src/part2/client/ip.py
+++ src/part2/client/ip.py
@@ -1,5 +1,5 @@
 import os
-frontend_ip = os.getenv("FRONTEND_IP", "http://localhost:8080")
+frontend_ip = os.getenv("FRONTEND_IP", "http://ec2-54-227-12-61.compute-1.amazonaws.com:8080")
 order_ip_1 = os.getenv("ORDER_IP_1", "http://localhost:8081")
 order_ip_2 = os.getenv("ORDER_IP_2", "http://localhost:8082")
 order_ip_3 = os.getenv("ORDER_IP_3", "http://localhost:8083")
